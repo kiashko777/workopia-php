cc94df4242e38670579c5bebe51e8a9edb05b701
kiashko777 <kiashko777@gmail.com>
2025-04-03 00:15:35 +0400

    Create partials
    partials created and loaded

--- helpers.php
+++ helpers.php
@@ -11,3 +11,39 @@ function basePath(string $path): string
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ *Load a view
+ *
+ * @param string $name
+ * @return void
+ */
+
+function loadView(string $name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+/**
+ *Load a partial
+ *
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial(string $name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name} not found!'";
+    }
+}
