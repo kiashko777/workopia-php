52e91cc5b27949b13f5b4b9cee1a50096e46edc6
kiashko777 <kiashko777@gmail.com>
2025-06-01 00:35:06 +0400

    fix bug middleware func
    authorize middleware functionality implemented

--- Framework/middleware/Authorize.php
+++ Framework/middleware/Authorize.php
@@ -1,35 +1,33 @@
 <?php
 
-namespace Framework\middleware;
+namespace Framework\Middleware;
 
 use Framework\Session;
 
 class Authorize
 {
     /**
-     *Check if the user is authenticated
+     * Check if user is authenticated
+     *
      * @return bool
      */
-
-    public function isAuthenticated(): bool
+    public function isAuthenticated()
     {
         return Session::has('user');
     }
 
     /**
-     *Handle the user request
+     * Handle the user's request
+     *
      * @param string $role
      * @return bool
      */
-
-    public function handle(string $role): bool
+    public function handle($role)
     {
         if ($role === 'guest' && $this->isAuthenticated()) {
             return redirect('/');
-        } else if ($role === 'auth' && !$this->isAuthenticated()) {
+        } elseif ($role === 'auth' && !$this->isAuthenticated()) {
             return redirect('/auth/login');
         }
     }
-
-
 }
