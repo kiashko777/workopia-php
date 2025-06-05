b7872715719d769257e3dec2c768af32fe8a9c12
kiashko777 <kiashko777@gmail.com>
2025-06-05 23:16:45 +0400

    search func
    search listings functionality implemented

--- App/views/partials/showcase-search.php
+++ App/views/partials/showcase-search.php
@@ -5,19 +5,23 @@
     <div class="overlay"></div>
     <div class="container mx-auto text-center z-10">
         <h2 class="text-4xl text-white font-bold mb-4">Find Your Dream Job</h2>
-        <form class="mb-4 block mx-5 md:mx-auto">
-            <input
-              type="text"
-              name="keywords"
-              placeholder="Keywords"
-              class="w-full md:w-auto mb-2 px-4 py-2 focus:outline-none"
-            />
-            <input
-              type="text"
-              name="location"
-              placeholder="Location"
-              class="w-full md:w-auto mb-2 px-4 py-2 focus:outline-none"
-            />
+        <form method="GET" action="/listings/search" class="mb-4 block mx-5 md:mx-auto">
+            <label>
+                <input
+                  type="text"
+                  name="keywords"
+                  placeholder="Keywords"
+                  class="w-full md:w-auto mb-2 px-4 py-2 focus:outline-none"
+                />
+            </label>
+            <label>
+                <input
+                  type="text"
+                  name="location"
+                  placeholder="Location"
+                  class="w-full md:w-auto mb-2 px-4 py-2 focus:outline-none"
+                />
+            </label>
             <button
               class="w-full md:w-auto bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 focus:outline-none"
             >
